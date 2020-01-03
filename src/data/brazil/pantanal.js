57458e263339762f9b5b1cba641e0a92b9b14fb6
Max Harding <maxharding4@gmail.com>
2020-01-03 08:35:29 +0000

    adds pantanal

--- src/data/brazil/pantanal.js
+++ src/data/brazil/pantanal.js
@@ -3,32 +3,30 @@ export const photos = [
     id: 1,
     country: 'brazil',
     location: 'pantanal',
-    name: 'Pantanal #1',
-    width: 1,
-    height: 1
+    name: 'Pantanal'
   },
   {
     id: 2,
     country: 'brazil',
     location: 'pantanal',
-    name: 'Pantanal #2',
-    width: 1,
-    height: 1
+    name: 'Pantanal'
   },
   {
     id: 3,
     country: 'brazil',
     location: 'pantanal',
-    name: 'Pantanal #3',
-    width: 1,
-    height: 1
+    name: 'Pantanal'
   },
   {
     id: 4,
     country: 'brazil',
     location: 'pantanal',
-    name: 'Pantanal #4',
-    width: 1,
-    height: 1
+    name: 'Pantanal'
+  },
+  {
+    id: 5,
+    country: 'brazil',
+    location: 'pantanal',
+    name: 'Pantanal'
   },
 ]
