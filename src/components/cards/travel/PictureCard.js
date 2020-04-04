eea0931b1f4a41ecd294a867c67a867f65e3011a
Max Harding <maxharding4@gmail.com>
2020-04-04 10:47:56 +0100

    center align picture cards

--- src/components/cards/travel/PictureCard.js
+++ src/components/cards/travel/PictureCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PictureCard = ({ id, country, location, name }) => {
   return (
-    <div className='bg-white-10 mw7 dib br3 pa1 ma2 bw2 shadow-5 v-top' card={name}>
+    <div className='bg-white-10 mw7 dib br3 pa1 ma2 bw2 shadow-5 v-mid' card={name}>
       <img alt={(`${location}-${id}`)} src={require(`../../../images/${country}/${location}/${location}-${id}.jpg`)} />
       <div>
         <h2>#{id} - {name}</h2>
